f1af43782a861a9c788086793a03f181db2e8053
mrkrntl <markbenedickteoxon.rantael@bicol-u.edu.ph>
2022-03-21 01:44:29 +0800

    Update - Moving Shapes

--- src/basicshape/BSPanel.java
+++ src/basicshape/BSPanel.java
@@ -10,6 +10,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
@@ -20,17 +21,27 @@ import javax.swing.JPanel;
 class BSPanel extends JPanel {
     
     // square
-    public int squareX = 50;
-    public int squareY = 50;
+    public int squareX = 100;
+    public int squareY = 200;
     public int squareW = 20;
     public int squareH = 20;
     
     //oval
-    public int OvalX = 100;
-    public int OvalY = 100;
+    public int OvalX = 150;
+    public int OvalY = 150;
     public int OvalW = 50;
     public int OvalH = 50;
     
+    //Ring
+    Random rn = new Random();
+    public int RingXmax = 580;
+    public int RingYmax = 280;
+    public int RingMin = 40;
+    public int RingX = RandomEvenGenerator(RingMin,RingXmax);
+    public int RingY = RandomEvenGenerator(RingMin,RingYmax);
+    
+    
+    //DIMENSION
     private int WIDTH = 640;
     private int HEIGHT = 360;
     
@@ -43,9 +54,21 @@ class BSPanel extends JPanel {
     
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //background
-        g.setColor(Color.black);
-        g.fillRect(0, 0, WIDTH, HEIGHT);
+        //title
+        g.setColor(Color.CYAN);
+        g.drawString("Activity 5", 10, 20);
+        //members
+        g.drawString("Members:", 10, 45);
+        g.drawString("Princess Joy Borlagdan", 10, 60);
+        g.drawString("Jevah Rea Montas", 10, 75);
+        g.drawString("Leomil Arvi Relorcasa", 10, 90);
+        g.drawString("Mark Benedick Rantael", 10, 105);
+        
+        //Instructrion
+        g.setColor(Color.YELLOW);
+        g.drawString("Intructions:",215,30);
+        g.drawString("Bring The Yellow Ball to the Red Ring using the small box!",295,30);
+
         //Square
         g.setColor(Color.RED);
         g.fillRect(squareX,squareY,squareW,squareH);
@@ -56,11 +79,27 @@ class BSPanel extends JPanel {
         g.fillOval(OvalX, OvalY, OvalW, OvalH);
         g.setColor(Color.WHITE);
         g.drawOval(OvalX, OvalY, OvalW, OvalH);
+        //Ring
+        g.setColor(Color.RED);
+        g.drawOval(RingX, RingY, 50, 50);
+       
+    }
+    
+    public int RandomEvenGenerator(int min, int max){
+        int RandomInt = 0;
+        while (true){
+            RandomInt = rn.nextInt(max - min + 1) + min;
+            if (RandomInt % 10 != 0) {
+                continue;
+            } else {
+            break;
+            }
+        }
+        return RandomInt;
     }
     
     public boolean hitEdgeX(int point) {
-        System.out.println(this.WIDTH);
-        System.out.println(point);
+       
         if(point >= (this.WIDTH - 20) || point <= 0) {
             return true;
         } else {
@@ -77,8 +116,8 @@ class BSPanel extends JPanel {
     }  
     
     public boolean checkMoveRightOvalX(int pointX, int pointY) {
-        if(pointY > 80 && pointY < 150) {
-            if (pointX == 90) {
+        if(pointY > OvalY-20 && pointY < OvalY+50) {
+            if (pointX == OvalX-10) {
                 return true;
             }
         }
@@ -86,8 +125,8 @@ class BSPanel extends JPanel {
     }
     
     public boolean checkMoveLeftOvalX(int pointX, int pointY) {
-        if(pointY > 80 && pointY < 150) {
-            if (pointX == 140) {
+        if(pointY > OvalY-20 && pointY < OvalY+50) {
+            if (pointX == OvalX+40) {
                 return true;
             }
         }
@@ -95,8 +134,8 @@ class BSPanel extends JPanel {
     }
     
     public boolean checkMoveDownOvalY(int pointX, int pointY) {
-        if(pointX > 80 && pointX < 150) {
-            if (pointY == 90) {
+        if(pointX > OvalX-20 && pointX < OvalX+50) {
+            if (pointY == OvalY-10) {
                 return true;
             }
         }
@@ -104,8 +143,8 @@ class BSPanel extends JPanel {
     }
 
     public boolean checkMoveUpOvalY(int pointX, int pointY) {
-        if(pointX > 80 && pointX < 150) {
-            if (pointY == 140) {
+        if(pointX > OvalX-20 && pointX < OvalX+50) {
+            if (pointY == OvalY+40) {
                 return true;
             }
         }
--- src/basicshape/BasicShape.java
+++ src/basicshape/BasicShape.java
@@ -31,76 +31,113 @@ public class BasicShape extends JFrame {
          @Override
          public void keyPressed(KeyEvent evt) {
             switch(evt.getKeyCode()) {
-               case KeyEvent.VK_LEFT:
-                  
+                
+               case KeyEvent.VK_LEFT:                 
                   predictionpoint = bspanel_canvas.squareX - 10;
                   
                   if(!bspanel_canvas.hitEdgeX(predictionpoint)) {
-                    //bspanel_canvas.squareX +=10;
                     if (!bspanel_canvas.checkMoveLeftOvalX(predictionpoint, bspanel_canvas.squareY)) {
                         bspanel_canvas.squareX -=10;
                         repaint();
+                    } else {
+                        if(!bspanel_canvas.hitEdgeX(bspanel_canvas.OvalX-30)) {    //30 because 10 for the border and 20 for the square to move around the oval
+                        bspanel_canvas.squareX -= 10;
+                        bspanel_canvas.OvalX -= 10;
+                        repaint();
+                        }
                     }
-                    //repaint();
+                    if (bspanel_canvas.OvalX == bspanel_canvas.RingX && bspanel_canvas.OvalY == bspanel_canvas.RingY) {
+                        JOptionPane.showMessageDialog(null, "You're the Best!","Activity 5 - Message",JOptionPane.PLAIN_MESSAGE);
+                        bspanel_canvas.RingX = bspanel_canvas.RandomEvenGenerator(bspanel_canvas.RingMin, bspanel_canvas.RingXmax);
+                        bspanel_canvas.RingY = bspanel_canvas.RandomEvenGenerator(bspanel_canvas.RingMin, bspanel_canvas.RingYmax);  
+                        repaint();
+                    }
                   }
-                  
-
-                  
                   break;
+                  
                case KeyEvent.VK_RIGHT:
-                   
                   predictionpoint = bspanel_canvas.squareX + 10; 
                   
+                  System.out.println(bspanel_canvas.OvalX+60);
                   if(!bspanel_canvas.hitEdgeX(predictionpoint)) {
-                    //bspanel_canvas.squareX +=10;
                     if (!bspanel_canvas.checkMoveRightOvalX(predictionpoint, bspanel_canvas.squareY)) {
                         bspanel_canvas.squareX +=10;
                         repaint();
+                    } else {
+                        if(!bspanel_canvas.hitEdgeX(bspanel_canvas.OvalX+60)) {    //60 because 20 for border and 40 for the square to move around the oval
+                        bspanel_canvas.squareX += 10;
+                        bspanel_canvas.OvalX += 10;
+                        repaint();
+                        }
                     }
-                    System.out.println("RIGHT");
-                    //repaint();
+                    if (bspanel_canvas.OvalX == bspanel_canvas.RingX && bspanel_canvas.OvalY == bspanel_canvas.RingY) {
+                        JOptionPane.showMessageDialog(null, "Threee Poooooints!","Activity 5 - Message",JOptionPane.PLAIN_MESSAGE);
+                        bspanel_canvas.RingX = bspanel_canvas.RandomEvenGenerator(bspanel_canvas.RingMin, bspanel_canvas.RingXmax);
+                        bspanel_canvas.RingY = bspanel_canvas.RandomEvenGenerator(bspanel_canvas.RingMin, bspanel_canvas.RingYmax);  
+                        repaint();
+                    }
                   }
-                  
-                  
-                  
                   break;
-               case KeyEvent.VK_UP:
-                   
-                  predictionpoint = bspanel_canvas.squareY - 10;
                   
-                  if(!bspanel_canvas.hitEdgeY(predictionpoint)) {
-                    //bspanel_canvas.squareY -=10;
+               case KeyEvent.VK_UP:                  
+                  predictionpoint = bspanel_canvas.squareY - 10;
+
+                  if(!bspanel_canvas.hitEdgeY(predictionpoint)) {              
                     if (!bspanel_canvas.checkMoveUpOvalY(bspanel_canvas.squareX, predictionpoint)) {
                         bspanel_canvas.squareY -= 10;
                         repaint();
+                    } else {
+                        if(!bspanel_canvas.hitEdgeY(bspanel_canvas.OvalY-30)) {    //30 because 10 for the border and 20 for the square to move around the oval
+                        bspanel_canvas.squareY -= 10;
+                        bspanel_canvas.OvalY -= 10;
+                        repaint();
+                        }
                     }
-                    //repaint();
-                  }
-                  break;
-               case KeyEvent.VK_DOWN:
-
-                  predictionpoint = bspanel_canvas.squareY + 10;
-                  
-                  if(!bspanel_canvas.hitEdgeY(predictionpoint)) {
-                    //bspanel_canvas.squareY +=10;
-                    if (!bspanel_canvas.checkMoveDownOvalY(bspanel_canvas.squareX, predictionpoint)) {
-                        bspanel_canvas.squareY +=10;
+                    if (bspanel_canvas.OvalX == bspanel_canvas.RingX && bspanel_canvas.OvalY == bspanel_canvas.RingY) {
+                        JOptionPane.showMessageDialog(null, "Ang Galing Mo!","Activity 5 - Message",JOptionPane.PLAIN_MESSAGE);
+                        bspanel_canvas.RingX = bspanel_canvas.RandomEvenGenerator(bspanel_canvas.RingMin, bspanel_canvas.RingXmax);
+                        bspanel_canvas.RingY = bspanel_canvas.RandomEvenGenerator(bspanel_canvas.RingMin, bspanel_canvas.RingYmax);  
                         repaint();
                     }
-                    System.out.println("UP");
-                    //repaint();
+                  }
+                  break;
+                  
+               case KeyEvent.VK_DOWN:
+                  predictionpoint = bspanel_canvas.squareY + 10;
+                
+                  if(!bspanel_canvas.hitEdgeY(predictionpoint)) {
+                    if (!bspanel_canvas.checkMoveDownOvalY(bspanel_canvas.squareX, predictionpoint)) {  
+                        bspanel_canvas.squareY += 10;
+                        repaint();
+                    } else {
+                        if(!bspanel_canvas.hitEdgeY(bspanel_canvas.OvalY+60)) {    //60 because 20 for border and 40 for the square to move around the oval
+                        bspanel_canvas.squareY += 10;
+                        bspanel_canvas.OvalY += 10;
+                        repaint();
+                        }
+                    }
+                    if (bspanel_canvas.OvalX == bspanel_canvas.RingX && bspanel_canvas.OvalY == bspanel_canvas.RingY) {
+                        JOptionPane.showMessageDialog(null, "You're Amazing","Activity 5 - Message",JOptionPane.PLAIN_MESSAGE);
+                        bspanel_canvas.RingX = bspanel_canvas.RandomEvenGenerator(bspanel_canvas.RingMin, bspanel_canvas.RingXmax);
+                        bspanel_canvas.RingY = bspanel_canvas.RandomEvenGenerator(bspanel_canvas.RingMin, bspanel_canvas.RingYmax);  
+                        repaint();
+                    }
                   }
                   break;
             }
             System.out.println("SquareX: " + Integer.toString(bspanel_canvas.squareX) + "  ||  OvalX: " + Integer.toString(bspanel_canvas.OvalX));
             System.out.println("SquareY: " + Integer.toString(bspanel_canvas.squareY) + "  ||  OvalY: " + Integer.toString(bspanel_canvas.OvalY));
+            System.out.println("RingX: " + bspanel_canvas.RingX + " || Ring Y: " + bspanel_canvas.RingY);
+            System.out.println("");
          }
       });
       
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Handle the CLOSE button
-      setTitle("Basic Shape");
+      setTitle("Activity 5 - Moving Shapes");
       pack();       // pack all the components in the JFrame
       setLocationRelativeTo(null);
+      bspanel_canvas.setBackground( Color.black );
+      setResizable(false);
       setVisible(true); // show it
       requestFocus();
       
